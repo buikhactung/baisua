5b52eeaefa86db413bcb3e84efa0961541c4f3f0
Your Name <you@example.com>
2019-09-26 17:29:08 +0700

    bai sua

---
+++ JS.js
@@ -0,0 +1,15 @@
+function convert(){
+    let inputA=document.getElementById("Money").value;
+    let from = document.getElementById("From").value;
+    let to = document.getElementById("To").value;
+    if(from == "VND" && to == "USD"){
+       let inputB = (inputA*23000);
+       alert(inputB);
+    }else if(from == "USD" && to == "VND"){
+        let inputB = (inputA/23000);
+        alert(inputB);
+    }else {
+        alert("ok");
+
+    }
+ }
