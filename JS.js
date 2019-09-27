50813664f40312e36a240670b33c1bb447b25276
Your Name <you@example.com>
2019-09-27 08:31:50 +0700

    nop bai tap

--- JS.js
+++ JS.js
@@ -1,12 +1,13 @@
 function convert(){
-    let inputA=document.getElementById("Money").value;
+    let inputA= (document.getElementById("Money").value);
     let from = document.getElementById("From").value;
-    let to = document.getElementById("To").value;
+    let to = document.getElementById( "To").value;
+    alert(to);
     if(from == "VND" && to == "USD"){
-       let inputB = (inputA*23000);
+       let inputB = (inputA/23000);
        alert(inputB);
     }else if(from == "USD" && to == "VND"){
-        let inputB = (inputA/23000);
+        let inputB = (inputA*23000);
         alert(inputB);
     }else {
         alert("ok");
